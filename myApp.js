a63dda4e6286f6ae6ada9e828c7dedd843268e87
Supreeth A L <supreethal40@gmail.com>
2025-10-18 13:25:47 +0530

    use yarn file

--- myApp.js
+++ myApp.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const helmet = require('helmet');
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 
 
